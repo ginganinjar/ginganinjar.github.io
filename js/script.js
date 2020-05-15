d64d13cbc7818809ebca1db25ecde0309ece39a0
David Sparrius <david@mstone.com.au>
2020-05-15 17:44:04 +1000

    boxes and stuff fixed

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
     if (x.style.display === "none") {
       x.style.display = "block";
       document.getElementById("down-arrow").innerHTML = "close";
-      window.scrollTo(0,document.body.scrollHeight);
+
     } else {
       document.getElementById("down-arrow").innerHTML = "REPO";
       x.style.display = "none";

5e49219abe047afc82592b806e20b84bfdb9b72e
David Sparrius <david@mstone.com.au>
2020-05-15 18:42:44 +1000

    footerfixed

--- js/script.js
+++ js/script.js
@@ -4,11 +4,12 @@
     if (x.style.display === "none") {
       x.style.display = "block";
       document.getElementById("down-arrow").innerHTML = "close";
+      document.getElementById("botomfooter").style.marginBottom = "-320px";
 
     } else {
       document.getElementById("down-arrow").innerHTML = "REPO";
       x.style.display = "none";
-   
+      document.getElementById("botomfooter").style.marginBottom = "-222px";
     }
   
   }

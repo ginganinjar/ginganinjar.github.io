319c45a8a9a2ded1cbd89ebe1b5c85efd24b2e00
David Sparrius <david@mstone.com.au>
2020-05-16 21:54:31 +1000

    portfolio

--- js/script.js
+++ js/script.js
@@ -4,13 +4,14 @@
     if (x.style.display === "none") {
       x.style.display = "block";
       document.getElementById("down-arrow").innerHTML = "close";
-  
+        document.getElementById("footer").style.display = "none";
+
 
 
     } else {
       document.getElementById("down-arrow").innerHTML = "REPO";
       x.style.display = "none";
-   
+      document.getElementById("footer").style.display = "block";
     }
   
   }
